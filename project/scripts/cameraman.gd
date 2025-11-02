b26fa0c79c8f4ef15fbd9b68c5b63a08a0d664bd
Don Juan Javier <townofdon@gmail.com>
2025-11-02 15:49:50 -0500

    separate cameraman from camera

--- project/scripts/cameraman.gd
+++ project/scripts/cameraman.gd
@@ -1,8 +1,10 @@
-extends Camera3D
+extends Node3D
 
 @export var acceleration := 13.0
 @export var moveSpeed := 5.0
 @export var lookSpeed := 5.0
+
+@onready var camera: Camera3D = %Camera3D
 
 var _is_mouse_in_window := false
 var move:Vector3 = Vector3.ZERO
@@ -12,8 +14,8 @@ var looking:bool = false
 
 func _ready() -> void:
 	# x and y are swapped here bc they represent the axis being rotated around.
-	look.x = transform.basis.get_euler().y / -0.001
-	look.y = transform.basis.get_euler().x / -0.001
+	look.x = camera.transform.basis.get_euler().y / -0.001
+	look.y = camera.transform.basis.get_euler().x / -0.001
 
 func _physics_process(delta: float) -> void:
 	if (Input.is_action_pressed("looking")):
@@ -26,7 +28,7 @@ func _physics_process(delta: float) -> void:
 	# Mouse movement.
 	if looking:
 		look.y = clamp(look.y, -1560, 1560)
-		transform.basis = Basis.from_euler(Vector3(look.y * -0.001, look.x * -0.001, 0))
+		camera.transform.basis = Basis.from_euler(Vector3(look.y * -0.001, look.x * -0.001, 0))
 
 	# Keyboard input
 	var inputH := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
@@ -35,10 +37,10 @@ func _physics_process(delta: float) -> void:
 		inputV -= 1.0
 	if Input.is_action_pressed("move_up"):
 		inputV += 1.0
-	var movement := transform.basis * (Vector3(inputH.x, inputV, inputH.y))
+	var movement := camera.transform.basis * (Vector3(inputH.x, inputV, inputH.y))
 	movement = Vector3(movement.x, movement.y, movement.z) * moveSpeed * 0.1
 	velocity = velocity.lerp(movement, 1 - exp(-acceleration * delta))
-	global_position = global_position + velocity
+	camera.global_position = camera.global_position + velocity
 
 
 func _input(event):
